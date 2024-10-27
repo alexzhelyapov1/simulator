1ba0c7ec9b356d46ff2431d3a4c0b6056655148b
Maria <you@example.com>
2024-10-27 18:49:30 +0300

    fixed types, casts, shifts

--- instructions_map/process_instrs.rb
+++ instructions_map/process_instrs.rb
@@ -7,19 +7,11 @@ class InstructionSet
   end
 
   def find_by_format(format)
-    matching_instructions = {}
-
-    @yaml_data['instructions'].each do |instruction_name, details|
-      if details['format'] == format
-        matching_instructions[instruction_name] = details
-      end
-    end
-    matching_instructions
+    @yaml_data['instructions'].select { |_, details| details['format'] == format }
   end
 
   def get_format_info(format)
     format_info = {}
-
     @yaml_data['formats'].each do |format_name, details|
       if format_name == format
         format_info = details
@@ -28,19 +20,57 @@ class InstructionSet
     format_info
   end
 
+  def calculate_mask(start_bit, end_bit)
+    (1 << (end_bit - start_bit + 1) - 1) << start_bit
+  end
+
+  def set_bits(value, start_bit)
+    value << start_bit
+  end
+
   def get_args(format)
     format_info = get_format_info(format)
-    format_args = {}
-    format_info.each do |arg_name, range|
+    format_info.each_with_object({}) do |(arg_name, range), format_args|
       start_bit, end_bit = range
-      mask = ((1 << (end_bit - start_bit + 1)) - 1) << start_bit
-      format_args[arg_name] = start_bit, mask
+      format_args[arg_name] = {start_bit: start_bit, end_bit: end_bit, mask: calculate_mask(start_bit, end_bit)}
     end
-    format_args
   end
+
+  def generate_c_code(template_path, output_path)
+    template = ERB.new(File.read(template_path))
+    result = template.result(binding)
+    File.write(output_path, result)
+  end
+
+  def generate_instruction_keys
+    @yaml_data['instructions'].each_with_object({}) do |(instruction_name, details), instruction_map|
+      key = create_instruction_key(details)
+      instruction_map[key] = instruction_name
+    end
+  end
+
+  def generate_map
+    instruction_map = generate_instruction_keys
+    cpp_map_entries = instruction_map.map do |key, instruction_name|
+      "{#{key}, #{instruction_name}},"
+    end.join("\n")
+    cpp_map_entries
+  end
+
+  def create_instruction_key(details)
+    format_details = @yaml_data['formats'][details['format']]
+    key = 0
+    ['opcode', 'f3', 'f7', 'shamt'].each do |field|
+      next unless details.key?(field) && format_details.key?(field)
+
+      start_bit, end_bit = format_details[field]
+      value = details[field]
+      key |= set_bits(value, start_bit)
+    end
+    key
+  end
+
 end
 
-template = ERB.new(File.read('generate_insts.erb')) 
-
-result = template.result(binding) 
-File.write("generated_functions.cpp", result)
+instruction_set = InstructionSet.new('riscv_instructions.yaml')
+instruction_set.generate_c_code('generate_insts.erb', 'generated_functions.cpp')
