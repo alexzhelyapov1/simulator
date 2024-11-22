bdf9438437a26126a45a6c949a1208ac9ed79038
Zhelyapov Aleksey <none@example.com>
2024-11-23 02:36:41 +0300

    Fixed loader buffer allocation

--- machine/hart.cpp
+++ machine/hart.cpp
@@ -1,26 +1,26 @@
 #include "hart.h"
-#include "machine.h"
 #include "gen_func.h"
+#include "machine.h"
 
 namespace Machine {
 
 RegValue Hart::getReg(const RegId &reg) { return Regfile[reg]; }
 
 void Hart::setReg(const RegId &reg, const RegValue &val) {
-  if (reg == 0) {
-    return;
-  }
-  Regfile[reg] = val;
+    if (reg == 0) {
+        return;
+    }
+    Regfile[reg] = val;
 }
 
 std::shared_ptr<Instr> Hart::decode(const Word &instrCode) {
-  auto inst = instCache->get(instrCode);
-  if (inst != nullptr) {
+    auto inst = instCache->get(instrCode);
+    if (inst != nullptr) {
+        return inst;
+    }
+    inst = std::make_shared<Instr>(instrCode);
+    instCache->put(inst);
     return inst;
-  }
-  inst = std::make_shared<Instr>(instrCode);
-  instCache->put(inst);
-  return inst;
 }
 
 void Hart::setPC(const RegValue &pc) { PC = pc; }
@@ -28,19 +28,19 @@ void Hart::setPC(const RegValue &pc) { PC = pc; }
 const RegValue &Hart::getPC() { return PC; }
 
 void Hart::RunSimpleInterpreterWithInstCache() {
-  while (true) {
-    auto instCode = loadMem<Word>(PC);
-    auto inst = decode(instCode);
-    inst->handler(*this, inst);
-    PC += sizeof(Word);
-  }
+    while (true) {
+        auto instCode = loadMem<Word>(PC);
+        auto inst = decode(instCode);
+        inst->handler(*this, inst);
+        PC += sizeof(Word);
+    }
 }
 
 void Hart::exceptionReturn() { throw std::runtime_error("EXCEPTION RETURN FROM HART"); }
 
 inline const RegValue &Hart::MMU(RegValue vaddress) {
-  // TODO insert MMU here
-  return std::move(vaddress);
+    // TODO insert MMU here
+    return std::move(vaddress);
 }
 
 } // namespace Machine
