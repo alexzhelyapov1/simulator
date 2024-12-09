903349cd12fc52c8c7ca2dac5c42331263c780b9
Zhelyapov Aleksey <none@example.com>
2024-12-10 01:28:00 +0300

    Working

--- loader/loader.cpp
+++ loader/loader.cpp
@@ -5,15 +5,15 @@
 
 namespace Loader {
 
-#if __x86_64__ || __ppc64__
+// #if __x86_64__ || __ppc64__
 typedef Elf64_Ehdr Elf_Ehdr;
 typedef Elf64_Phdr Elf_Phdr;
 typedef Elf64_Addr Elf_Addr;
-#else
-typedef Elf32_Ehdr Elf_Ehdr;
-typedef Elf32_Phdr Elf_Phdr;
-typedef Elf32_Addr Elf_Addr;
-#endif
+// #else
+// typedef Elf32_Ehdr Elf_Ehdr;
+// typedef Elf32_Phdr Elf_Phdr;
+// typedef Elf32_Addr Elf_Addr;
+// #endif
 
 uint64_t Loader::loadElf(const std::string &path) const {
     std::ifstream file(path, std::ios::binary);
--- machine/machine.h
+++ machine/machine.h
@@ -1,6 +1,7 @@
 #include "mem.h"
 #include <memory>
 #include <vector>
+#include <unordered_map>
 #ifndef MACHINE_H
 #define MACHINE_H
 
@@ -21,6 +22,7 @@ class Machine {
     std::vector<std::shared_ptr<Hart>> harts;
     std::shared_ptr<Mem> mem;
     std::shared_ptr<Loader::Loader> loader;
+    std::unordered_map<uint64_t, uint64_t> page_table;
 
   public:
     Machine(RegValue memSize) { mem = std::make_shared<Mem>(memSize); }
