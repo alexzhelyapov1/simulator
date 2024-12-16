581184b28c1b8455f822a6ffaaee9f611130854f
Maria <you@example.com>
2024-12-16 16:29:57 +0300

    Try to fix CSR instrs

--- test/test_timers.s
+++ test/test_timers.s
@@ -1,15 +1,11 @@
 .global _start
 _start:
-    # Определяем константы для регистров
     .equ MSTATUS, 4
     
-    # Тестирование CSRRSI
-    addi x1, x0, 2 #  устанавливаемый бит 
-    csrrsi x2, MSTATUS, 2 #Читаем mstatus, выставляем бит 2, старое значение записываем в x2
+    addi x1, x0, 2 
+    csrrsi x2, MSTATUS, 2 
 
-    # Проверка
-    csrr x2, MSTATUS #проверяем значение после выставления бита
+    csrr x2, MSTATUS
     
-    # Выход
     addi x7, x0, 93
     sret
